1af204ad6b64cd8e4e01311946867532dfd30e71
Raraw97 <aurora.esthi@mhs.itenas.ac.id>
2023-11-09 17:05:33 +0700

    first commit

---
+++ Kalkulator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // definisikan widget yang akan diinteraksikan
+    // 1. edit text - bil 1
+    // val--->variabel bersifat tetap
+    // var--->variabel bersifat dinamis(value)
+
+
+}
---
+++ Login/app/src/main/java/com/example/login/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    private Edit etUsername, etPassword;
+    public Button btnLogin;
+
+    private String username = "Aurora";
+    private String password = "Admin";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         EditText etUsername =  ;
+
+    }
+}
